3d9c0745878a6dc6dfd094ff9810bc4fc4856d48
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-28 21:01:33 +0300

    fixed epoch time to humanreadable date problem

--- model/item.go
+++ model/item.go
@@ -23,51 +23,52 @@ type Item struct {
 	URL         string `json:"url"`
 }
 
+// FormattedTime function converts epoch time to human readable format
+func (i Item) FormattedTime(epoch int64) time.Time {
+	strTime := strconv.FormatInt(epoch, 10)
+	itemDate, err := strconv.ParseInt(strTime, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return time.Unix(itemDate, 0)
+}
+
 // FormattedStory formats our response to show meaningful output to user
 func (i Item) FormattedStory() string {
 	story := fmt.Sprintf(
-		"\nBy: %s\nID: %d\nComment Count: %d\nScore: %d\nTitle: %s\nURL: %s\n",
-		i.By, i.ID, i.Descendants, i.Score, i.Title, i.URL)
+		"\nTime: %v\nBy: %s\nID: %d\nComment Count: %d\nScore: %d\nTitle: %s\nURL: %s\n",
+		i.FormattedTime(i.Time), i.By, i.ID, i.Descendants, i.Score, i.Title, i.URL)
 	return story
 }
 
 // FormattedPoll formats our response to show meaningful output to user
 func (i Item) FormattedPoll() string {
 	poll := fmt.Sprintf(
-		"\nBy: %s\nComment Count: %d\nID: %d\nScore: %d\nText: %s\nTitle: %s\n",
-		i.By, i.Descendants, i.ID, i.Score, html.UnescapeString(i.Text), i.Title)
+		"\nTime: %v\nBy: %s\nComment Count: %d\nID: %d\nScore: %d\nText: %s\nTitle: %s\n",
+		i.FormattedTime(i.Time), i.By, i.Descendants, i.ID, i.Score, html.UnescapeString(i.Text), i.Title)
 	return poll
 }
 
 // FormattedJob formats our response to show meaningful output to user
 func (i Item) FormattedJob() string {
 	job := fmt.Sprintf(
-		"\nBy: %s\nID: %d\nScore: %d\nText: %s\nTitle: %s\nURL: %s\n",
-		i.By, i.ID, i.Score, html.UnescapeString(i.Text), i.Title, i.URL)
+		"\nTime: %v\nBy: %s\nID: %d\nScore: %d\nText: %s\nTitle: %s\nURL: %s\n",
+		i.FormattedTime(i.Time), i.By, i.ID, i.Score, html.UnescapeString(i.Text), i.Title, i.URL)
 	return job
 }
 
 // FormattedComment formats our response to show meaningful output to user
 func (i Item) FormattedComment() string {
 	comment := fmt.Sprintf(
-		"\nBy: %s\nID: %d\nParentID: %d\nText: %s\n",
-		i.By, i.ID, i.Parent, html.UnescapeString(i.Text))
+		"\nTime: %v\nBy: %s\nID: %d\nParentID: %d\nText: %s\n",
+		i.FormattedTime(i.Time), i.By, i.ID, i.Parent, html.UnescapeString(i.Text))
 	return comment
 }
 
 // FormattedAsk formats our response to show meaningful output to user
 func (i Item) FormattedAsk() string {
 	ask := fmt.Sprintf(
-		"\nBy: %s\nComment Count: %d\nID: %d\nScore: %d\nText: %s\nTitle: %s\n",
-		i.By, i.Descendants, i.ID, i.Score, html.UnescapeString(i.Text), i.Title)
+		"\nTime: %v\nBy: %s\nComment Count: %d\nID: %d\nScore: %d\nText: %s\nTitle: %s\n",
+		i.FormattedTime(i.Time), i.By, i.Descendants, i.ID, i.Score, html.UnescapeString(i.Text), i.Title)
 	return ask
-}
-
-// FormattedTime function converts epoch time to human readable format
-func (i Item) FormattedTime(epoch int64) time.Time {
-	a, err := strconv.ParseInt(string(epoch), 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	return time.Unix(a, 0)
 }
