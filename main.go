b9c45c1ca77ccd76f69c4562193512a9b648ae5b
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-29 18:30:00 +0300

    get top stories flag implemented with basic function

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ const BaseURL = "https://hacker-news.firebaseio.com/v0/item/"
 
 func main() {
 	getLastItem := flag.Bool("last", false, "display last post, it can be story, comment, job, poll or ask")
+	getTopStories := flag.Bool("top", false, "display top stories, you can pass arguments until 500. default first 100 stories will shown.")
 	flag.Parse()
 	if *getLastItem {
 		response, err := http.Get("https://hacker-news.firebaseio.com/v0/maxitem.json")
@@ -26,19 +27,18 @@ func main() {
 		client.GetLastItem(BaseURL, *response)
 	}
 
-	getTopStories := flag.Bool("top", false, "display top stories, you can pass arguments until 500. default first 100 stories will shown.")
-
-	topResponse, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
-	if err != nil {
-		log.Fatalln(err)
+	if *getTopStories {
+		response, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		topStory, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var topStories []int64
+		json.Unmarshal(topStory, &topStories)
+		fmt.Println(topStories)
 	}
-	fmt.Println(topResponse.Body)
-	topStory, err := ioutil.ReadAll(topResponse.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	var topStories []int64
-	json.Unmarshal(topStory, &topStories)
-	fmt.Println(topStories[0])
 
 }
