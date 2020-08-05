8c842e349e64b67eba23e3d65ae6c84d10a1229d
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-05 19:54:00 +0300

    update show table function

--- client/fetch_item.go
+++ client/fetch_item.go
@@ -21,7 +21,6 @@ func DisplayItem(output string, typeOfPost string, itemURL string) {
 // GetItem function returns the item that related ID
 func GetItem(itemID int64) {
 	var ItemResponse model.Item
-	itemresponselist := []model.Item{}
 	itemURL := fmt.Sprintf("%s%d%s", BaseURL, itemID, ".json")
 	item, err := http.Get(itemURL)
 
@@ -37,5 +36,5 @@ func GetItem(itemID int64) {
 	if error != nil {
 		fmt.Println("Something bad happened: ", error)
 	}
-	fmt.Printf("%T ", itemresponselist)
+	fmt.Println(ItemResponse)
 }
