011e5a0cd9d777cadb41e89d9c9a86914552af3d
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-06 16:27:27 +0300

    for now i can fetch ids from topstory function

--- client/fetch_top_stories.go
+++ client/fetch_top_stories.go
@@ -6,14 +6,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/hackernews-go-cli/model"
 )
 
 // GetTopStories returns top stories
 func GetTopStories(num int) {
-	var ItemResponse model.Item
-	itemresponselist := []model.Item{}
+	//var ItemResponse model.Item
+	//itemresponselist := []model.Item{}
+	//topStoriesList := []model.Item{}
 	response, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
 	if err != nil {
 		log.Fatalln(err)
@@ -29,23 +28,27 @@ func GetTopStories(num int) {
 		fmt.Println("You have exceeded the max number of item, if you want to list all the item, use this as an argument: ", len(topStories))
 	} else {
 		for i := 0; i < num; i++ {
-			itemURL := fmt.Sprintf("%s%d%s", BaseURL, topStories[i], ".json")
-			item, err := http.Get(itemURL)
+			// itemURL := fmt.Sprintf("%s%d%s", BaseURL, topStories[i], ".json")
+			// item, err := http.Get(itemURL)
 
-			if err != nil {
-				log.Fatalln(err)
-			}
-			defer item.Body.Close()
-			itemBody, err := ioutil.ReadAll(item.Body)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			error := json.Unmarshal(itemBody, &ItemResponse)
-			if error != nil {
-				fmt.Println("Something bad happened: ", error)
-			}
-			itemresponselist = append(itemresponselist, ItemResponse)
+			// if err != nil {
+			// 	log.Fatalln(err)
+			// }
+			// defer item.Body.Close()
+			// itemBody, err := ioutil.ReadAll(item.Body)
+			// if err != nil {
+			// 	log.Fatalln(err)
+			// }
+			// error := json.Unmarshal(itemBody, &ItemResponse)
+			// if error != nil {
+			// 	fmt.Println("Something bad happened: ", error)
+			// }
+			//element := GetItem(topStories[i][0])
+			//topStoriesList = append(topStoriesList, GetItem(topStories[i]))
+			GetItem(topStories[i])
+			fmt.Println(topStories[i])
+			fmt.Printf("%T", topStories[i])
 		}
-		ShowTable(itemresponselist)
+		//ShowTable(topStoriesList)
 	}
 }
