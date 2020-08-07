b646e418ff67e2c70c69309f81776fe4de8845d7
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-07 16:07:45 +0300

    refactor model file

--- model/item.go
+++ model/item.go
@@ -1,8 +1,8 @@
 package model
 
 import (
-	"fmt"
-	"html" // html.UnescapeString unescapes entities like "&lt;" to become "<".
+
+	// html.UnescapeString unescapes entities like "&lt;" to become "<".
 	"strconv"
 	"time"
 )
@@ -32,44 +32,4 @@ func (i Item) FormattedTime(epoch int64) string {
 	}
 	t := time.Unix(itemDate, 0)
 	return t.Format("2006-01-02 15:04:05")
-}
-
-// FormattedStory formats our response to show meaningful output to user
-func (i Item) FormattedStory() string {
-	story := fmt.Sprintf(
-		"\nTime: %v\nBy: %s\nID: %d\nComment Count: %d\nScore: %d\nTitle: %s\nURL: %s\n",
-		i.FormattedTime(i.Time), i.By, i.ID, i.Descendants, i.Score, i.Title, i.URL)
-	return story
-}
-
-// FormattedPoll formats our response to show meaningful output to user
-func (i Item) FormattedPoll() string {
-	poll := fmt.Sprintf(
-		"\nTime: %v\nBy: %s\nComment Count: %d\nID: %d\nScore: %d\nText: %s\nTitle: %s\n",
-		i.FormattedTime(i.Time), i.By, i.Descendants, i.ID, i.Score, html.UnescapeString(i.Text), i.Title)
-	return poll
-}
-
-// FormattedJob formats our response to show meaningful output to user
-func (i Item) FormattedJob() string {
-	job := fmt.Sprintf(
-		"\nTime: %v\nBy: %s\nID: %d\nScore: %d\nText: %s\nTitle: %s\nURL: %s\n",
-		i.FormattedTime(i.Time), i.By, i.ID, i.Score, html.UnescapeString(i.Text), i.Title, i.URL)
-	return job
-}
-
-// FormattedComment formats our response to show meaningful output to user
-func (i Item) FormattedComment() string {
-	comment := fmt.Sprintf(
-		"\nTime: %v\nBy: %s\nID: %d\nParentID: %d\nText: %s\n",
-		i.FormattedTime(i.Time), i.By, i.ID, i.Parent, html.UnescapeString(i.Text))
-	return comment
-}
-
-// FormattedAsk formats our response to show meaningful output to user
-func (i Item) FormattedAsk() string {
-	ask := fmt.Sprintf(
-		"\nTime: %v\nBy: %s\nComment Count: %d\nID: %d\nScore: %d\nText: %s\nTitle: %s\n",
-		i.FormattedTime(i.Time), i.By, i.Descendants, i.ID, i.Score, html.UnescapeString(i.Text), i.Title)
-	return ask
 }
