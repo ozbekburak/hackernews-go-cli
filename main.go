9c2137eca53890895a37999e089ef5ec0c9aac82
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-13 21:42:30 +0300

    stop showing last item

--- main.go
+++ main.go
@@ -8,17 +8,15 @@ import (
 
 func main() {
 	var storyCount int
-	getLastItem := flag.Bool("last", false, "display last post, it can be story, comment, job, poll or ask")
+	//getLastItem := flag.Bool("last", false, "display last post, it can be story, comment, job, poll or ask")
 	flag.IntVar(&storyCount, "top", 1, "display top stories, you can pass arguments until 500")
 	flag.Parse()
 
-	if *getLastItem {
-		client.GetLastItem()
-	}
+	//if *getLastItem {
+	//	client.GetLastItem()
+	//}
 
 	if storyCount > 0 && storyCount <= 500 {
-		if !*getLastItem {
-			client.GetTopStories(storyCount)
-		}
+		client.GetTopStories(storyCount)
 	}
 }
