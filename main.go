125a632e1f49d0bbe4196c42eb30db6997420305
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-13 22:10:49 +0300

    change name conventions and clear some comments

--- main.go
+++ main.go
@@ -6,33 +6,31 @@ import (
 	"github.com/hackernews-go-cli/client"
 )
 
+var (
+	storyNum int
+	askNum   int
+	jobNum   int
+	showNum  int
+)
+
 func main() {
-	var storyCount int
-	var askCount int
-	var jobCount int
-	var showCount int
-	//getLastItem := flag.Bool("last", false, "display last post, it can be story, comment, job, poll or ask")
-	flag.IntVar(&storyCount, "top", 1, "display top stories, you can pass arguments until 500")
-	flag.IntVar(&askCount, "ask", 1, "display ask stories, you can pass arguments until 200")
-	flag.IntVar(&jobCount, "job", 1, "display job stories, you can pass arguments until 200")
-	flag.IntVar(&showCount, "show", 1, "display job stories, you can pass arguments until 200")
+	flag.IntVar(&storyNum, "top", 1, "display top stories, you can pass arguments until 500")
+	flag.IntVar(&askNum, "ask", 1, "display ask stories, you can pass arguments until 200")
+	flag.IntVar(&jobNum, "job", 1, "display job stories, you can pass arguments until 200")
+	flag.IntVar(&showNum, "show", 1, "display job stories, you can pass arguments until 200")
 
 	flag.Parse()
 
-	//if *getLastItem {
-	//	client.GetLastItem()
-	//}
-
-	if storyCount > 0 && storyCount <= 500 {
-		client.GetStories(storyCount, "story")
+	if storyNum > 0 && storyNum <= 500 {
+		client.GetStories(storyNum, "story")
 	}
-	if askCount > 0 && askCount <= 200 {
-		client.GetStories(askCount, "ask")
+	if askNum > 0 && askNum <= 200 {
+		client.GetStories(askNum, "ask")
 	}
-	if jobCount > 0 && jobCount <= 200 {
-		client.GetStories(jobCount, "job")
+	if jobNum > 0 && jobNum <= 200 {
+		client.GetStories(jobNum, "job")
 	}
-	if showCount > 0 && showCount <= 200 {
-		client.GetStories(showCount, "show")
+	if showNum > 0 && showNum <= 200 {
+		client.GetStories(showNum, "show")
 	}
 }
