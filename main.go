e327c67c3bfb750ed4d04d9a07602f77a27402d8
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-17 21:47:24 +0300

    add else condition to flag check

--- main.go
+++ main.go
@@ -16,17 +16,15 @@ var (
 
 func main() {
 	// maybe it can be done with sub-commands
-	flag.IntVar(&storyNum, "top", 1, "display top stories, you can pass arguments until 500")
-	flag.IntVar(&askNum, "ask", 1, "display ask stories, you can pass arguments until 200")
-	flag.IntVar(&jobNum, "job", 1, "display job stories, you can pass arguments until 200")
-	flag.IntVar(&showNum, "show", 1, "display job stories, you can pass arguments until 200")
+	flag.IntVar(&storyNum, "top", 0, "display top stories, you can pass arguments until 500")
+	flag.IntVar(&askNum, "ask", 0, "display ask stories, you can pass arguments until 200")
+	flag.IntVar(&jobNum, "job", 0, "display job stories, you can pass arguments until 200")
+	flag.IntVar(&showNum, "show", 0, "display job stories, you can pass arguments until 200")
 
 	flag.Parse()
 
 	if storyNum > 0 && storyNum <= 500 {
-
 		client.FetchStories(storyNum, "topstories")
-		fmt.Println(storyNum)
 	}
 	if askNum > 0 && askNum <= 200 {
 		client.FetchStories(askNum, "askstories")
@@ -36,6 +34,8 @@ func main() {
 	}
 	if showNum > 0 && showNum <= 200 {
 		client.FetchStories(showNum, "showstories")
+	} else if storyNum <= 0 && askNum <= 0 && jobNum <= 0 && showNum <= 0 {
+		fmt.Println("No argument found! Please use --help to see which flags you can use!")
 	}
 
 }
