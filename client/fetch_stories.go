844dcec3c256a81ed5e13ce1b09374a131d350e9
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-17 20:08:50 +0300

    trying to fetch different types of stories, for now just show and top

--- client/fetch_stories.go
+++ client/fetch_stories.go
@@ -15,24 +15,43 @@ const baseStoryURL = "https://hacker-news.firebaseio.com/v0/"
 // GetStories returns top stories
 func GetStories(num int, storyType string) {
 	storyURL := fmt.Sprintf("%s%s%s", baseStoryURL, storyType, ".json")
-	itemArray := []model.Story{}
+	var storyArray []model.Story
+	var showArray []model.Story
+	// var askArray []model.Ask
+	// var jobArray []model.Job
+
 	response, err := http.Get(storyURL)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer response.Body.Close()
-	topStory, err := ioutil.ReadAll(response.Body)
+	storyList, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	var topStories []int64
-	json.Unmarshal(topStory, &topStories)
+	json.Unmarshal(storyList, &topStories)
 	if len(topStories) < num {
 		fmt.Println("You have exceeded the max number of item, if you want to list all the item, use this as an argument: ", len(topStories))
 	} else {
 		for i := 0; i < num; i++ {
-			itemArray = append(itemArray, GetItem(topStories[i]))
+			switch storyType {
+			case "topstories":
+				storyArray = append(storyArray, GetItem(topStories[i]))
+			case "showstories":
+				showArray = append(showArray, GetItem(topStories[i]))
+				// case "askstories":
+				// 	askArray = append(askArray, GetItem(topStories[i]))
+				// case "jobstories":
+				// 	jobArray = append(jobArray, GetItem(topStories[i]))
+			}
+
 		}
-		ShowTable(itemArray)
+		switch storyType {
+		case "topstories":
+			ShowTable(storyArray)
+		case "showstories":
+			ShowTable(showArray)
+		}
 	}
 }
