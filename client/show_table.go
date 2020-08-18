4e706b53d6aed9abc96f0bf3110e8470c1380531
Burak Kaan <ozbekburakkaan@outlook.com>
2020-08-18 22:38:17 +0300

    play with wordwrapper

--- client/show_table.go
+++ client/show_table.go
@@ -5,9 +5,9 @@ import (
 	"html"
 
 	"github.com/hackernews-go-cli/model"
-	"github.com/mitchellh/go-wordwrap"
 
 	"github.com/alexeyco/simpletable"
+	"github.com/mitchellh/go-wordwrap"
 )
 
 // ShowTable creates a table with related data
@@ -15,7 +15,6 @@ func ShowTable(item []model.Story, storyType string) {
 	table := simpletable.New()
 	table.Header = &simpletable.Header{
 		Cells: []*simpletable.Cell{
-			// {Align: simpletable.AlignCenter, Text: "ID"},
 			{Align: simpletable.AlignCenter, Text: "By"},
 			{Align: simpletable.AlignCenter, Text: "Score"},
 			{Align: simpletable.AlignCenter, Text: "Time"},
@@ -26,13 +25,12 @@ func ShowTable(item []model.Story, storyType string) {
 
 	for _, x := range item {
 		r := []*simpletable.Cell{
-			// {Text: fmt.Sprintf("%d", x.ID)},
 			{Text: x.By},
 			{Text: fmt.Sprintf("%d", x.Score)},
 			{Text: fmt.Sprintf("%v", x.FormattedTime(x.Time))},
-			{Text: fmt.Sprintf("%v", wordwrap.WrapString(html.UnescapeString(x.Title), 75))},
-			// WrapString library works for only whitespace, so it can not be used for URL.
-			// Maybe custom url shortener fit in here!
+			{Text: fmt.Sprintf("%v", wordwrap.WrapString(html.UnescapeString(x.Title), 50))},
+			// wordwrap library works for only whitespace, so it can not be used for URL. This is why i dont display ID!
+			// maybe custom url shortener fit in here!
 			{Text: x.URL}}
 		table.Body.Cells = append(table.Body.Cells, r)
 	}
